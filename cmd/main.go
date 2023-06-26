1e40f19d9b7810754367ba4da7369b943999b455
Mizumoto <mizumotokunn@gmail.com>
2023-06-26 10:05:32 +0900

    config path fix

--- cmd/main.go
+++ cmd/main.go
@@ -17,7 +17,7 @@ var (
 )
 
 func init() {
-	flag.StringVar(&flagconf, "conf", "..", "config path, eg: -conf config.yaml")
+	flag.StringVar(&flagconf, "conf", "../configs", "config path, eg: -conf config.yaml")
 }
 
 func main() {
