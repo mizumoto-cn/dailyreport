688e52c4659d48a2ce745f28c6058d314e1cc021
Mizumoto <mizumotokunn@gmail.com>
2023-07-05 10:22:30 +0900

    added powered by to default

--- pkg/util/smtp.go
+++ pkg/util/smtp.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/go-kratos/kratos/v2/log"
 	"github.com/mizumoto-cn/dailyreport/conf"
 	"gopkg.in/gomail.v2"
 )
@@ -29,11 +30,20 @@ func NewEmailFormatter(conf *conf.SmtpDialer) EmailFormatter {
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		line := scanner.Text()
-		template += line
+		template += line + "\n"
 	}
+	poweredBy := `
+    <div class="signature" style="margin-top: 20px; text-align: right; background-color: #eee;">
+      <p>
+      Powered by <a href="https://github.com/mizumoto-cn/dailyreport">@mizumoto-cn/dailyreport</a>
+      <img src="https://github.com/mizumoto-cn/mizumoto-cn/raw/main/image.png" width="16" height="16" style="margin-top: 4px"/>
+      </p>
+    </div>
+`
 	return func(to []string, token ...string) *gomail.Message {
 		currentDate := time.Now().Format("20060102")
 		body := fmt.Sprintf(template, convertToAnySlice(token)...)
+		addPoweredBy(&body, poweredBy)
 		m := gomail.NewMessage()
 		m.SetHeader("From", conf.Username)
 		m.SetHeader("To", to...)
@@ -49,4 +59,31 @@ func convertToAnySlice(slice []string) []any {
 		is[i] = s
 	}
 	return is
+}
+
+func addPoweredBy(body *string, poweredBy string) {
+	// if contains </div> then add poweredBy before last </div>
+	// else if contains </body> then add poweredBy before </body>
+	// else add poweredBy before </html>
+	// else add poweredBy at the end of body
+	if i := lastIndexOf(*body, "</div>"); i != -1 {
+		*body = (*body)[:i] + poweredBy + (*body)[i:]
+	} else if i := lastIndexOf(*body, "</body>"); i != -1 {
+		*body = (*body)[:i] + poweredBy + (*body)[i:]
+	} else if i := lastIndexOf(*body, "</html>"); i != -1 {
+		*body = (*body)[:i] + poweredBy + (*body)[i:]
+	} else {
+		*body += poweredBy
+	}
+	log.Debugf("body: %s", *body)
+}
+
+func lastIndexOf(s string, substr string) int {
+	index := -1
+	for i := 0; i < len(s)-len(substr); i++ {
+		if s[i:i+len(substr)] == substr {
+			index = i
+		}
+	}
+	return index
 }
